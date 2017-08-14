4b6852b337d8943014f47a45cf8980f41c3edd68
Jerrad Monagan <jerradmonagan@gmail.com>
2017-08-14 09:06:21 -0400

    added views for steps 1.2 and 1.3

---
+++ VMS homework/Controllers/MainController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VMS_homework.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ManageVol()
+        {
+            return View();
+        }
+        public ActionResult ManageOpp()
+        {
+            return View();
+        }
+    }
+}
