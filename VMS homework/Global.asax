326e795977e48c4bb21c5d7164d181ad93c208ad
Jerrad Monagan <jerradmonagan@gmail.com>
2017-08-14 07:43:07 -0400

    Add project files.

---
+++ VMS homework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VMS_homework.MvcApplication" Language="C#" %>
---
+++ VMS homework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VMS_homework.Startup))]
+namespace VMS_homework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
