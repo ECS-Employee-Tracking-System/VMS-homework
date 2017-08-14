3feec51730f0aef3704e8003eb16e336ed31d133
Jerrad Monagan <jerradmonagan@gmail.com>
2017-08-14 12:16:36 -0400

    Completed all filters in step 1.5 with the exception of filter A. Filter A requires an OR function to be added.

---
+++ VMS homework/Controllers/SearchController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VMS_homework.Models;
+
+namespace VMS_homework.Controllers
+{
+    public class SearchController : Controller
+    {
+        VMSEntities db = new VMSEntities();
+        // GET: Search
+
+        //for future use to search any other addition status that would be used under approval status
+        public ActionResult Index(String searching)
+        {
+            var status = from s in db.volunteers
+                         select s;
+            if (!String.IsNullOrEmpty(searching))
+            {
+                status = status.Where(s => s.ApprovalStatus.Contains(searching));
+            }
+            return View(status.ToList());
+
+        }
+        //returns all users that have an Approved or pending status
+        public ActionResult FilterA()
+        {
+            var status = from s in db.volunteers
+                         select s;
+           
+                status = status.Where(s => s.ApprovalStatus.Contains("Approved"));
+            
+            return View(status.ToList());
+        }
+
+        //returns all users that have an Approved status
+        public ActionResult FilterB()
+        {
+            var status = from s in db.volunteers
+                         select s;
+
+            status = status.Where(s => s.ApprovalStatus.Equals("Approved"));
+
+            return View(status.ToList());
+        }
+
+        //returns all users that have a Pending Status
+        public ActionResult FilterC()
+        {
+            var status = from s in db.volunteers
+                         select s;
+
+            status = status.Where(s => s.ApprovalStatus.Contains("Pending"));
+
+            return View(status.ToList());
+        }
+
+        //returns all users that have a Disaprroved Status
+        public ActionResult FilterD()
+        {
+            var status = from s in db.volunteers
+                         select s;
+
+            status = status.Where(s => s.ApprovalStatus.Contains("Disapproved"));
+
+            return View(status.ToList());
+        }
+
+        //returns all users that have an inactive status
+        public ActionResult FilterE()
+        {
+            var status = from s in db.volunteers
+                         select s;
+
+            status = status.Where(s => s.ApprovalStatus.Contains("Inactive"));
+
+            return View(status.ToList());
+        }
+
+        //returns all users in the system
+        public ActionResult FilterF()
+        {
+            var status = from s in db.volunteers
+                         select s;
+
+            status = status.Where(s => s.ApprovalStatus.Contains(""));
+
+            return View(status.ToList());
+        }
+    }
+}
